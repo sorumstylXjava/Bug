857009960625df1589855994fa823861100d4adf
sorumstylXjava <sorumstyl@gmail.com>
2026-02-23 09:04:48 +0700

    Update LoginActivity.kt

--- app/src/main/java/com/example/kernvar/LoginActivity.kt
+++ app/src/main/java/com/example/kernvar/LoginActivity.kt
@@ -1,1 +1,15 @@
+package com.example.kernvar
 
+import android:content.intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.toast
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+  override fun onCreate(savedinstanceState:Bundle?) {
+    super.onCreate(savedinstanceState)
+    setContentView(R.layout.activity_login)
+
+    val usernameEditText: EditText = findViewByld(R.id.usernameEditText)
